c495df4a350f2fe6d5803a6656113f2e1ecd5ade
kkamara <kelvinkamara@protonmail.com>
2021-03-31 18:37:09 +0100

    Handle cors

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const { helpersConfig } = require("./config");
 
 const cookieParser = require("cookie-parser");
 const express = require("express");
+var cors = require('cors');
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 app.use(cookieParser());
+app.use(cors());
 
 // routes
 // app.get("/", middleware.csrfProtection, middleware.render("pages/index"));
