96058996fbfda7e669ac862304c17715d66de542
kkamara <kelvinkamara@protonmail.com>
2021-04-02 12:08:58 +0100

    Stateful page design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,81 @@
-import react, { Fragment } from 'react';
+import { Fragment, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 import { APP_URL } from './constants';
 import Loader from './components/Loader';
 
-const handleButtonClick = (e, bc) => {
-    console.log('in button click handler');
-    bc.postMessage('This is a test message.');
-};
-
 function App() {
+    const [name, setName] = useState('');
+    const [loading, setLoading] = useState(true);
+
     const bc = new BroadcastChannel('test_channel');
     bc.onmessage = function (ev) { console.log(ev); }
 
+    /** 
+     * @const closeBCConnection 
+     * @param {event} e
+     * @param {BroadcastChannel} bc
+     */
+    const closeBCConnection = (e, bc) => {
+        console.log('in button click handler', e, bc);
+        // bc.postMessage('This is a test message.');
+        bc.close();
+    };
+    
+    /** 
+    * @const handleItemSubmit 
+    * @param {event} e
+    * @param {BroadcastChannel} bc
+    */
+    const handleItemSubmit = (e, bc) => {
+        console.log('in item submit handler');
+    }
+
+    /** render */
     return (
         <Fragment>
             <Loader />
             <button 
-                onClick={e => handleButtonClick(e, bc)}
+                onClick={e => closeBCConnection(e, bc)}
                 className="py-2 px-4 bg-green-500 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75"
             >
-                Click me
+                Close connection
             </button>
-            <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+            <form className="max-w-md mx-auto">
                 <div className="md:flex">
-                    <div className="p-8">
-                        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Case study</div>
-                        <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding customers for your new business</a>
-                        <p className="mt-2 text-gray-500">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
+                    <label 
+                        htmlFor="name"
+                        className="mt-2"
+                    >
+                        Name:
+                    </label>
+                    <input 
+                        name="name" 
+                        type="text" 
+                        className="rounded text-pink-500 ml-10"
+                        onChange={e => setName(e.target.value)}
+                        value={name}
+                    />
+                    <button 
+                        className='ml-10'
+                        onClick={e => handleItemSubmit(e, bc)}
+                    >
+                        <FontAwesomeIcon size="2x" className="plus-icon" icon={faPlus} />
+                    </button>
+                </div>
+            </form>
+            <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+                <div className="p-8">
+                    <div 
+                        className="uppercase tracking-wide text-sm text-indigo-500 font-semibold grid grid-cols-10"
+                    >
+                        <div className="col-span-8" >Case study</div>
+                        <div className="col-end">
+                            <button>
+                                <FontAwesomeIcon size="2x" className="trash-alt-icon" icon={faTrashAlt} />
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
