dc56f7753dfb617f05cb005f529c1c390a3d3cc0
kkamara <kelvinkamara@protonmail.com>
2021-04-02 22:35:20 +0100

    Refactor CreateItem to its own component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
 import { Fragment, useState, useEffect } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
 
 import API_ROUTES from './api-routes';
 import Loader from './components/Loader';
 import Items from './components/Items';
+import CreateItem from './components/CreateItem';
 
 /** @func App */
 function App() {
@@ -78,31 +77,12 @@ function App() {
             >
                 Close connection
             </button>
-            <form className="max-w-md mx-auto">
-                <div className="md:flex">
-                    <label 
-                        htmlFor="name"
-                        className="mt-2"
-                    >
-                        Name:
-                    </label>
-                    <input 
-                        minLength={3}
-                        maxLength={20}
-                        name="name" 
-                        type="text" 
-                        className="rounded text-pink-500 ml-10"
-                        onChange={e => setName(e.target.value)}
-                        value={name}
-                    />
-                    <button 
-                        className='ml-10'
-                        onClick={async e => handleItemSubmit(e, bc)}
-                    >
-                        <FontAwesomeIcon size="2x" className="plus-icon" icon={faPlus} />
-                    </button>
-                </div>
-            </form>
+            <CreateItem
+                name={name}
+                setName={setName}
+                handleItemSubmit={handleItemSubmit}
+                broadcastChannel={bc}
+            />
             <Items 
                 items={data} 
                 setItems={setData}
--- frontend/src/components/CreateItem.js
+++ frontend/src/components/CreateItem.js
@@ -1,5 +1,37 @@
-import react from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
-const AddItem = () => {}
+const CreateItem = ({
+    name,
+    setName,
+    handleItemSubmit,
+    broadcastChannel,
+}) => (
+    <form className="max-w-md mx-auto">
+        <div className="md:flex">
+            <label 
+                htmlFor="name"
+                className="mt-2"
+            >
+                Name:
+            </label>
+            <input 
+                minLength={3}
+                maxLength={20}
+                name="name" 
+                type="text" 
+                className="rounded text-pink-500 ml-10"
+                onChange={e => setName(e.target.value)}
+                value={name}
+            />
+            <button 
+                className='ml-10'
+                onClick={async e => handleItemSubmit(e, broadcastChannel)}
+            >
+                <FontAwesomeIcon size="2x" className="plus-icon" icon={faPlus} />
+            </button>
+        </div>
+    </form>
+);
 
-export default AddItem;
+export default CreateItem;
