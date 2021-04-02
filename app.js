c5ac61d0db32812476f7107222c16d9a80f1de77
kkamara <kelvinkamara@protonmail.com>
2021-04-03 00:36:25 +0100

    Serving static react app from backend

--- app.js
+++ app.js
@@ -13,19 +13,22 @@ const slugify = require('slugify');
 const cookieParser = require("cookie-parser");
 const sanitize = require('sanitize');
 const express = require("express");
-var cors = require('cors');
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static("public"));
+const path = require('path');
+
+const buildPath = path.join(__dirname, 'frontend', 'build');
+app.use(express.static(buildPath));
+
+app.use(express.urlencoded({ extended: true }));
 app.use(sanitize.middleware);
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.setHeader("Content-Type", "application/json");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization, x-id, Content-Length, X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next();
 });
 if (appConfig.appDebug === true) {
@@ -78,6 +81,9 @@ router.post('/items', async (req, res) => {
 
 router.get('/items', async (req, res) => {
     const data = await getAll();
+    if (data.length === 1) {
+        return res.send(JSON.stringify([ ...data ]));
+    }
     return res.send(JSON.stringify(data));
 });
 
